f0a0bd9e95591e1941104a55e9f6a639844a982c
Mankgwanyane Tlaka <mankgwanyane@tlaka.xyz>
2020-09-02 14:55:25 +0200

    Add modules
    1. Patient
    2. Control
    3. Admin
    4. Doctor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'patient', loadChildren: () => import('./layouts/patient/patient.module').then(m => m.PatientModule) },
+  { path: 'control', loadChildren: () => import('./layouts/control/control.module').then(m => m.ControlModule) },
+  { path: 'admin', loadChildren: () => import('./layouts/admin/admin.module').then(m => m.AdminModule) },
+  { path: 'doctor', loadChildren: () => import('./layouts/doctor/doctor.module').then(m => m.DoctorModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
