47720ac18dd792cab101927bf91be4177e0e2702
Mankgwanyane Tlaka <mankgwanyane@tlaka.xyz>
2020-09-08 05:52:01 +0200

    List centres

--- src/app/layouts/super/services/super.service.ts
+++ src/app/layouts/super/services/super.service.ts
@@ -1,6 +1,7 @@
 import { environment } from './../../../../environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Centre } from '../types/super-data-types';
 
 @Injectable({
   providedIn: 'root'
@@ -20,7 +21,7 @@ export class SuperService {
   }
 
   getAllRolesAndPrivileges() {
-    return this._httpClient.get(this.endpointBase.concat("Application/Roles/GetAll"), { reportProgress: true, observe: "events" })
+    return this._httpClient.get(this.endpointBase.concat("Application/Roles/GetAll"),{ reportProgress: true, observe: "events" })
   }
   createRoleAndPrivileges(payload) {
     return this._httpClient.post(this.endpointBase.concat("Application/Roles/Create"), payload, { reportProgress: true, observe: "events" })
@@ -32,7 +33,7 @@ export class SuperService {
   }
 
   getAllCentres(){
-    return this._httpClient.get(this.endpointBase.concat("Application/Centres/GetAll"), { reportProgress: true, observe: "events" })
+    return this._httpClient.get<Centre[]>(this.endpointBase.concat("Application/Centres/GetAll"),{ reportProgress: true, observe: "events" })
 
   }
 
