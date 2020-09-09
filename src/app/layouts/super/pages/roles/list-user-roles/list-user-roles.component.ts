5a8464a299202fb55c947465a92ce0bb642ce92a
Mankgwanyane Tlaka <mankgwanyane@tlaka.xyz>
2020-09-09 10:22:54 +0200

    final comit

--- src/app/layouts/super/pages/roles/list-user-roles/list-user-roles.component.ts
+++ src/app/layouts/super/pages/roles/list-user-roles/list-user-roles.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
 import { Router } from '@angular/router';
+import { SuperService } from '../../../services/super.service';
+import { HttpEventType } from '@angular/common/http';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-list-user-roles',
@@ -10,14 +13,26 @@ import { Router } from '@angular/router';
   styleUrls: ['./list-user-roles.component.scss']
 })
 export class ListUserRolesComponent implements OnInit {
-  displayedColumns: string[] = ['name', 'description', 'actions'];
+  displayedColumns: string[] = ['roleName', 'actions'];
   dataSource;
   roles: Role[] = [];
   role: Role;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
-  constructor(private _router: Router) { }
+  constructor(private _router: Router,private _superService:SuperService) {
+    this._superService.getAllRolesAndPrivileges().subscribe(event=>{
+      if (event.type === HttpEventType.Sent) {
+
+      }
+      if (event.type === HttpEventType.Response) {
+        this.roles = event.body as Role[];
+        this.dataSource = new MatTableDataSource<Role>(this.roles);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator =this.paginator;
+      }
+    })
+   }
 
   ngOnInit(): void {
   }
